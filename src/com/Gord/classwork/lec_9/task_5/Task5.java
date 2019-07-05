c68949442ba14164c793c2faa797407f090cf8ea
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-05 11:39:10 +0300

    Update Task5.java

--- src/com/Gord/classwork/lec_9/task_5/Task5.java
+++ src/com/Gord/classwork/lec_9/task_5/Task5.java
@@ -2,8 +2,11 @@ package com.Gord.classwork.lec_9.task_5;
 
 public class Task5 {
     public static void main(String[] args) {
+        // str -> wordsAsString
         String str = "Green. red. blue. yellow";
         System.out.println(str);
+        
+        //имя переменной с большой буквы!
         String Strr = str.replace(".", ",");
         System.out.println(Strr);
     }
