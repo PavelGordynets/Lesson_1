a3653105929f9655c36b2efe95cf1075701c3b6c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-05 11:56:43 +0300

    Update Task12.java

--- src/com/Gord/classwork/lec_9/task_12/Task12.java
+++ src/com/Gord/classwork/lec_9/task_12/Task12.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Task12 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        // плохое название переменной
         int next;
         do {
             System.out.println("Введите колличество строк: ");
@@ -26,6 +27,7 @@ public class Task12 {
 
         }
 //Здесь происходит сортировка (сортировка пузырьком):
+        // не нужно оставлять такие комментарии, вынеси этот код в отдельный метод
         for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j].length() > arr[j + 1].length()) {
@@ -36,6 +38,7 @@ public class Task12 {
             }
         }
         System.out.println("\n" + "Результат сортировки: ");
+        // не ошибка, но Arrays.toString() сюда очень хорошо подходит
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
