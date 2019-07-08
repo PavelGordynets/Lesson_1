6abc101b3703f946e96483e280bf6cd89985c444
PavelGordynets <P.Gordynets@gmail.com>
2019-07-08 19:35:30 +0300

    Исправил

--- src/com/Gord/classwork/lec_10/task_5/AutomaticCloseCar.java
+++ src/com/Gord/classwork/lec_10/task_5/AutomaticCloseCar.java
@@ -1,32 +1,30 @@
 package com.Gord.classwork.lec_10.task_5;
 
 public class AutomaticCloseCar {
-    // поля класса должны быть приватными
-    int distanceGpsFromCar = 0;
-    boolean signaling = false;
-    int maximumDistanceForAutomaticCloseCar = 5;
+    private int distanceGpsFromCar;
+    private boolean signaling;
+    private int maximumDistanceForAutomaticCloseCar;
 
-    // все эти параметры передаются через конструктор, а не указываются по умолчанию.   
-   // public AutomaticCloseCar()  -> public AutomaticCloseCar(int distanceGpsFromCar, boolean signaling,  int maximumDistanceForAutomaticCloseCar) 
-    public AutomaticCloseCar() {
-        //эти строки сейчас не имеют смысла
+    public AutomaticCloseCar(int distanceGpsFromCar, boolean signaling, int maximumDistanceForAutomaticCloseCar) {
         this.distanceGpsFromCar = distanceGpsFromCar;
         this.signaling = signaling;
         this.maximumDistanceForAutomaticCloseCar = maximumDistanceForAutomaticCloseCar;
     }
 
-    // крайне редко методы оставляют с дефолтным модификатором. Обычно паблик или приватный
-    boolean AutomaticCloseCar() {
+    public int distanceFromCar() {
+        for (int i = distanceGpsFromCar; i < maximumDistanceForAutomaticCloseCar; i++) {
+            distanceGpsFromCar++;
+            System.out.println("Вы отошли на " + distanceGpsFromCar + " м от машины");
+        }
+        return distanceGpsFromCar;
+    }
+    public boolean AutomaticCloseCar(){
         if (distanceGpsFromCar == maximumDistanceForAutomaticCloseCar) {
             signaling = true;
-            
-            // этот if всегда будет верным, т.к. чуть выше ты сетаешь true в signaling
-            // if(signaling == true) -> if (signaling) {}
-            if (signaling == true) {
+            if (signaling) {
                 System.out.println("Включена сигнализация");
             }
         }
         return signaling;
     }
 }
-
--- src/com/Gord/classwork/lec_10/task_5/Task5.java
+++ src/com/Gord/classwork/lec_10/task_5/Task5.java
@@ -2,14 +2,9 @@ package com.Gord.classwork.lec_10.task_5;
 
 public class Task5 {
     public static void main(String[] args) {
-        AutomaticCloseCar signaling = new AutomaticCloseCar();
-
+        AutomaticCloseCar signaling = new AutomaticCloseCar(4, false, 5);
         try {
-            for (int i = 0; i < signaling.maximumDistanceForAutomaticCloseCar; i++) {
-                signaling.distanceGpsFromCar++;
-                System.out.println("Вы отошли на " + signaling.distanceGpsFromCar + " м от машины");
-            }
-// здесь абзац не нужен
+            signaling.distanceFromCar();
         } finally {
             signaling.AutomaticCloseCar();
         }
